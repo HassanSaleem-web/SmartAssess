20ba8c8ff7494723b47c12ab6eacf5bf6061d8bd
Hassan Saleem <smuhammadhassan39@gmail.com>
2025-08-03 20:09:08 +0500

    second commit

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -27,7 +27,7 @@ export default function SignUp() {
     setMessage(null);
 
     try {
-      const res = await fetch("http://localhost:5001/api/register", {
+      const res = await fetch("https://backendregister.onrender.com/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
